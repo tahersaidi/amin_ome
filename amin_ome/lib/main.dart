be0f8c7f1f024295d4b50c8e493e756de56954a9
tahersaidi <tahersaidi1z21@gmail.com>
2025-05-16 15:04:23 +0100

    Add files via upload

---
+++ amin_ome/lib/main.dart
@@ -0,0 +1,24 @@
+﻿import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amin Secure App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Welcome to Amin Secure App!'),
+        ),
+      ),
+    );
+  }
+}
