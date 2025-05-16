097965c56f9cf878413c36eb235138d3632c23cd
tahersaidi <tahersaidi1z21@gmail.com>
2025-05-16 14:50:30 +0100

    Add files via upload

---
+++ amin_ome/lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Amin Bank',
+    home: Scaffold(
+      appBar: AppBar(title: Text('Amin Bank')),
+      body: Center(child: Text('© 2025 TaherSaidi Co.')),
+    ),
+  ));
+}
